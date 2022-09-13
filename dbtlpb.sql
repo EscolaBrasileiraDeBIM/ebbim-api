109fd4ae816e1082470af3be434c9fda443faf5e
Thiago Dias <79672937+ThiagoNDias@users.noreply.github.com>
2022-09-13 17:01:24 -0300

    Atualização no Banco de Dados
    Adicionadas as tabelas de 'emails' e 'telefones', com suas relações com 'pessoas'

--- dbtlpb.sql
+++ dbtlpb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 13-Set-2022 às 17:11
+-- Tempo de geração: 13-Set-2022 às 21:58
 -- Versão do servidor: 10.4.24-MariaDB
 -- versão do PHP: 8.1.6
 
@@ -24,6 +24,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `emails`
+--
+
+CREATE TABLE `emails` (
+  `id` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `principal` bit(1) NOT NULL,
+  `idpessoas` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `pessoas`
 --
 
@@ -38,9 +51,31 @@ CREATE TABLE `pessoas` (
   `cnpj` varchar(14) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `telefone`
+--
+
+CREATE TABLE `telefone` (
+  `id` int(11) NOT NULL,
+  `ddi` int(3) DEFAULT NULL,
+  `ddd` int(3) DEFAULT NULL,
+  `numero` int(11) DEFAULT NULL,
+  `tipo` enum('celular','residencial','comercial','outros') DEFAULT NULL,
+  `idpessoas` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `emails`
+--
+ALTER TABLE `emails`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idpessoas` (`idpessoas`);
 
 --
 -- Índices para tabela `pessoas`
@@ -52,14 +87,49 @@ ALTER TABLE `pessoas`
   ADD UNIQUE KEY `cnpj` (`cnpj`);
 
 --
+-- Índices para tabela `telefone`
+--
+ALTER TABLE `telefone`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idpessoas` (`idpessoas`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `emails`
+--
+ALTER TABLE `emails`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `pessoas`
 --
 ALTER TABLE `pessoas`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `telefone`
+--
+ALTER TABLE `telefone`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `emails`
+--
+ALTER TABLE `emails`
+  ADD CONSTRAINT `emails_ibfk_1` FOREIGN KEY (`idpessoas`) REFERENCES `pessoas` (`id`);
+
+--
+-- Limitadores para a tabela `telefone`
+--
+ALTER TABLE `telefone`
+  ADD CONSTRAINT `telefone_ibfk_1` FOREIGN KEY (`idpessoas`) REFERENCES `pessoas` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
