3212254caeb50799a8961429eb4a627d52c927ca
Thiago Dias <79672937+ThiagoNDias@users.noreply.github.com>
2022-09-15 15:12:27 -0300

    Correções
    Aplicadas algumas correções de acordo com o arquivo que já estava no Github

--- db_ebbimt.sql
+++ db_ebbimt.sql
@@ -6,24 +6,24 @@ use db_ebbim;
 
 create table aa_pessoas (
 	cd_pessoa int(11) not null,
-	nm_pessoa varchar(50) not null,
-    nm_apelido varchar(50),
-    nm_razao_social varchar(50),
+	nm_pessoa varchar(30),
+    nm_apelido varchar(30),
+    nm_razao_social varchar(30),
     dt_nascimento date,
     -- estrangeiro registra se é ou não estrangeiro, enquanto a nacionalidade registra qual o gentílico do país onde a pessoa nasceu
     ic_estrangeiro boolean,
     nm_nacionalidade varchar(22) default 'brasileiro',
     -- gênero inicialmente é masculino ou feminino
-    ic_genero varchar(20),
+    ic_genero varchar(10),
     ic_pf boolean,
     cd_cpf_cnpj varchar(14) unique,
-    cd_rg int(11) unique,
+    cd_rg varchar(11) unique,
     cd_inscricao_estadual int(9) unique,
     cd_inscricao_municipal int(11) unique,
     -- fornecedor é se a pessoa fornece algo, como por exemplo alimentos, remédios, produtos de limpeza, e etc
-    nm_fornecedor varchar(50),
-    nm_area_atuacao varchar(50),
-    nm_empresa varchar(50),
+    nm_fornecedor varchar(30),
+    nm_area_atuacao varchar(30),
+    nm_empresa varchar(30),
     nm_funcao varchar(30),
     ds_comentario varchar(140),
 	primary key (cd_pessoa)
@@ -35,22 +35,21 @@ create table ab_emails (
     ic_principal boolean,
     cd_pessoa int(11),
 	primary key (cd_email),
-    constraint fk_pessoa_email
+    constraint fk_email_pessoa
 		foreign key (cd_pessoa)
 		references aa_pessoas(cd_pessoa)
 ) default charset utf8mb4;
 
 create table ac_telefones (
 	cd_telefone int(11) not null,
+    nm_ddi int(3) default 51,
+    nm_ddd int(3),
+    nm_numero int(11),
     -- o tipo seria celular, comercial, residencial, outros ...
-    nm_tipo varchar(11),
-    cd_ddi int(3) default 51,
-    cd_ddd int(3),
-    cd_numero int(15),
-    cd_ramal int(5),
+    ic_tipo varchar(11),
     cd_pessoa int(11),
 	primary key (cd_telefone),
-    constraint fk_pessoa_telefone
+    constraint fk_telefone_pessoa
 		foreign key (cd_pessoa)
 		references aa_pessoas(cd_pessoa)
 ) default charset utf8mb4;
@@ -59,7 +58,7 @@ create table ad_colaboradores(
 	cd_colaborador int(11) not null,
     cd_pessoa int(11),
     primary key (cd_colaborador),
-    constraint fk_pessoa_colaborador
+    constraint fk_colaborador_pessoa
 		foreign key (cd_pessoa)
 		references aa_pessoas(cd_pessoa)
 ) default charset utf8mb4;
