503e2bda214fdc62acb187e856d3165081b1a660
Thiago Dias <79672937+ThiagoNDias@users.noreply.github.com>
2022-12-01 17:24:40 -0300

    Tabelas para inserir cursos e outros ajustes
    Adicionadas as tabelas de colaboradores, tipo de contrato, tipo de produto e ficha de instrutor, além de ajustes nas tabelas existentes para incluir chaves estrangeiras e novos campos, além de corrigir campos existentes

--- db_ebbim.sql
+++ db_ebbim.sql
@@ -48,34 +48,65 @@ create table ab_email(
     primary key (cd_email),
     constraint fk_pessoas_email foreign key (cd_pessoa) references aa_pessoas(cd_pessoa)
 );
+
+create table ad_colaboradores(
+	cd_colaborador varchar(10),
+    ic_ativo boolean,
+    ic_instrutor_ativo boolean,
+    ic_instrutor_tipo boolean,
+    cd_pessoa varchar(10),
+    primary key (cd_colaborador),
+    constraint fk_pessoas_colaboradores foreign key (cd_pessoa) references aa_pessoas(cd_pessoa)
+);
+
+create table cd_tipo_de_contrato(
+	cd_tipo_de_contrato int(1) unique,
+	ds_comercializacao varchar(20),
+	ds_conteudo varchar(20),
+	ds_descricao varchar(20),
+	ds_desenvolvimento varchar(20),
+	ds_instrucao varchar(20),
+	ds_transmissao varchar(20)
+);
+
+create table ce_tipo_de_produto(
+	sg_tipo_de_produto varchar(2),
+	ds_descricao varchar(20),
+    primary key (sg_tipo_de_produto)
+);
+
 create table ca_cursos(
 	cd_curso varchar(10),
 	cd_curso3dig varchar(3) unique,
-    -- cd_data é um cálculo no FM (c_tabelaPrecoAtiva) que pega o ID primário da tabela cb_data_reajuste
-	cd_data varchar(10) unique,
+    nm_oficial varchar(50),
+    nm_status varchar(15),
 	-- dc_interno é o código completo do curso, criado a partir de outros dados. Exemplos PT-CR-01-4DV-A-08, PT-CR-01-VWR-B-08, PT-CR-02-BLD-A-24, e etc
 	dc_interno varchar(20),
-    nm_oficial varchar(50),
 	-- nm_interno é o apelido do curso
     nm_interno varchar(50),
     nm_link_site varchar(50),
-    nm_status varchar(15),
     sg_idioma varchar(2),
     sg_tipo_de_produto varchar(2),
     cd_tipo_de_contrato int(1),
     sg_produto varchar(3),
     cd_versao varchar(1),
     vl_horas_aula int(2),
-    primary key (cd_curso)
+    -- cd_data é um cálculo no FM (c_tabelaPrecoAtiva) que pega o ID primário da tabela cb_data_reajuste
+	cd_data varchar(10) unique,
+    primary key (cd_curso),
+    constraint fk_cursos_tipo_de_contrato foreign key (cd_tipo_de_contrato) references cd_tipo_de_contrato(cd_tipo_de_contrato),
+    constraint fk_cursos_tipo_de_produto foreign key (sg_tipo_de_produto) references ce_tipo_de_produto(sg_tipo_de_produto)
 );
+
 create table cb_data_reajuste(
 	cd_data varchar(10),
 	dt_inicio date,
 	dt_fim date,
-	cd_curso3dig varchar(3) unique,
+	cd_curso3dig varchar(3),
     primary key (cd_data),
     constraint fk_cursos_data foreign key (cd_curso3dig) references ca_cursos(cd_curso3dig)
 );
+
 create table cc_preco_base(
 	cd_preco varchar(10),
 	dt_atual date,
@@ -96,11 +127,23 @@ create table cc_preco_base(
     primary key (cd_preco),
     constraint fk_cursos_preco foreign key (cd_data) references ca_cursos(cd_data)
 );
+
 create table cc_preco_base_hist(
 	cd_preco_hist varchar(10),
 	dt_inicio date,
 	dt_fim date,
 	cd_data varchar(10) unique,
     primary key (cd_preco_hist),
-    constraint fk_cursos_preco foreign key (cd_data) references ca_cursos(cd_data)
+    constraint fk_cursos_hist foreign key (cd_data) references ca_cursos(cd_data)
+);
+
+create table cf_ficha_de_instrutor(
+	cd_ficha varchar(10),
+	cd_pessoa varchar(10) not null,
+	cd_colaborador varchar(10),
+    cd_curso3dig varchar(3),
+    primary key (cd_ficha),
+    constraint fk_pessoas_ficha_de_instrutor foreign key (cd_pessoa) references aa_pessoas(cd_pessoa),
+    constraint fk_colaboradores_ficha_de_instrutor foreign key (cd_colaborador) references ad_colaboradores(cd_colaborador),
+    constraint fk_cursos_ficha_de_instrutor foreign key (cd_curso3dig) references ca_cursos(cd_curso3dig)
 );
